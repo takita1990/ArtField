925aaf7cf7326e895a8d53fdaaf4bec5619f8b6d
vagrant <vagrant@localhost.localdomain>
2020-12-10 05:10:53 +0000

    add ranking

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -5,7 +5,7 @@ class WorksController < ApplicationController
   	@search_genre = Genre.where(id: params[:genre_id],valid_status:true)
   	@genres = Genre.all
     @works = Work.joins(:genre).where("genres.valid_status = ?",true)
-    #byebug
+    @all_ranks = Work.find(Favorite.group(:work_id).order('count(work_id) desc').limit(3).pluck(:work_id))
   end
 
   def show
